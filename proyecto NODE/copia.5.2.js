f6666ddab294dc706a27a3e80179a7d0cd54de7f
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-10-25 13:19:57 +0200

    subida nueva funcion al proyecto 5.2

---
+++ proyecto NODE/copia.5.2.js
@@ -0,0 +1,13 @@
+let suma = (num1, num2) => num1 + num2; 
+
+let resta = (num1, num2) => num1 - num2; 
+
+let producto = (num1, num2) => num1 * num2; 
+
+let division = (num1, num2) => num1 / num2; 
+
+let cuadrado = (num1) => num1.Math.Pow();
+
+
+module.exports = { suma, resta, producto, division, cuadrado };
+
